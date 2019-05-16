36325a4c94f5d4253c81548bfb6cc93e12ccb708
Nicolás Gomez <nco_0.sys@hotmail.com>
2019-05-15 21:58:42 -0400

    :tada: incremento 1

--- Laboratorio-9/Form1.Designer.cs
+++ Laboratorio-9/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 8;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(231, 56);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 8;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(300, 300);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
 
--- Laboratorio-9/Form1.cs
+++ Laboratorio-9/Form1.cs
@@ -12,9 +12,148 @@ namespace Laboratorio_9
 {
     public partial class Form1 : Form
     {
+        // Atributos para guardar filas y columnas, constante para no
+        // poder alterar su valor
+        private const int FILAS = 8;
+        private const int COLUMNAS = 8;
+
+        // Información de cada celda
+        Button[,] buttons;
+        bool[,] bombmons;
+
         public Form1()
         {
             InitializeComponent();
+
+            buttons = new Button[FILAS, COLUMNAS];
+            bombmons = new bool[FILAS, COLUMNAS];
+
+            // Agregaremos los botones con código, para no hacerlo 1 a 1...
+            for(int fila = 0; fila < FILAS; fila++)
+            {
+                for(int columna = 0; columna < COLUMNAS; columna++)
+                {
+                    Button button = new Button();
+                    button.Dock = DockStyle.Fill;
+                    button.Margin = new Padding(0, 0, 0, 0);
+                    button.Padding = new Padding(0, 0, 0, 0);
+                    button.FlatStyle = FlatStyle.Popup;
+                    button.FlatAppearance.BorderSize = 1;
+                    button.Click += cell_Click;
+                    tableLayoutPanel1.Controls.Add(button, columna, fila);
+                    buttons[fila, columna] = button;
+                }
+            }
+
+            // Bombmons
+            for (int fila = 0; fila < FILAS; fila++)
+                for (int columna = 0; columna < COLUMNAS; columna++)
+                    bombmons[fila, columna] = false;
+
+            // Creamos los 10 bombmons al azar
+            Random random = new Random();
+            int creados = 0;
+            while(creados < 10)
+            {
+                int fila = random.Next(FILAS);
+                int columna = random.Next(COLUMNAS);
+                bool existeUnBombmon = bombmons[fila, columna];
+                if (!existeUnBombmon)
+                {
+                    bombmons[fila, columna] = true;
+                    buttons[fila, columna].Text = "b";
+                    creados++;
+                }
+            }
+        }
+
+        private void cell_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            int filaBoton = 0;
+            int columnaBoton = 0;
+
+            // Buscamos la posición del botón
+            for (int fila = 0; fila < FILAS; fila++)
+            {
+                for (int columna = 0; columna < COLUMNAS; columna++)
+                {
+                    if (boton == buttons[fila, columna])
+                    {
+                        filaBoton = fila;
+                        columnaBoton = columna;
+                        break;
+                    }
+                }
+            }
+
+            // Vemos si en la posición existe un bombmon
+            if (ExisteBombmon(filaBoton, columnaBoton))
+            {
+                MostrarBombmons();
+                DeshabilitarBotones();
+                MessageBox.Show("Perdiste!! :c");
+            }
+            else
+            {
+                // Estilos para que no volvamos a apretarlo
+                boton.Enabled = false;
+                boton.BackColor = Color.Green;
+                boton.Text = ContarBombmons(filaBoton, columnaBoton).ToString();
+            }
+        }
+
+        private bool ExisteBombmon(int fila, int columna)
+        {
+            return bombmons[fila, columna];
+        }
+
+        private void MostrarBombmons()
+        {
+            for (int fila = 0; fila < FILAS; fila++)
+            {
+                for (int columna = 0; columna < COLUMNAS; columna++)
+                {
+                    if (bombmons[fila, columna])
+                    {
+                        buttons[fila, columna].BackColor = Color.Red;
+                    }
+                }
+            }
+        }
+
+        private void DeshabilitarBotones()
+        {
+            for (int fila = 0; fila < FILAS; fila++)
+            {
+                for (int columna = 0; columna < COLUMNAS; columna++)
+                {
+                    buttons[fila, columna].Enabled = false;
+                }
+            }
+        }
+
+        private int ContarBombmons(int filaCentral, int columnaCentral)
+        {
+            int cantidad = 0;
+            for(int fila = filaCentral - 1; fila <= filaCentral + 1; fila++)
+            {
+                for(int columna = columnaCentral - 1; columna <= columnaCentral + 1; columna++)
+                {
+                    // Verificamos si la posición es válida...
+                    bool filaValida = fila >= 0 && fila < FILAS;
+                    bool columnaValida = columna >= 0 && columna < COLUMNAS;
+                    if (filaValida && columnaValida)
+                    {
+                        if (bombmons[fila, columna])
+                        {
+                            // encontramos uno!
+                            cantidad += 1;
+                        }
+                    }
+                }
+            }
+            return cantidad;
         }
     }
 }
